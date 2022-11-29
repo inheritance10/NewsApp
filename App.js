1d892fd2a25ca75b2f26613e727084bfe4ae7120
inheritance10 <ali552001a@gmail.com>
2022-11-30 02:30:15 +0300

    News app ilk yukleme

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , FlatList} from 'react-native';
+import news_data from './news_data.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>News App</Text>
+
+        <FlatList data={news_data} renderItem={({item}) => <Text>{item.title}</Text>}
+
+        />
+      </View>
     </View>
   );
 }
@@ -14,7 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 35
   },
 });
